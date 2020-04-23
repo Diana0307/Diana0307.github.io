0bd931a2009a4dbb0c5e8ea2eb7cbd7bc9641d5f
Diana0307 <63189310+Diana0307@users.noreply.github.com>
2020-04-23 18:22:43 -0500

    Initial commit

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+  navigator.serviceWorker.register('./sw.js')
+    .then(reg => console.log('Registro de SW exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
