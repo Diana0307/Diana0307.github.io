46d31b8a523a00e4a9ca68700e2c0d90e1ab454b
Diana0307 <63189310+Diana0307@users.noreply.github.com>
2020-04-23 19:26:09 -0500

    Initial commit

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 var CACHE_NAME = 'v1_cache_geolocalizacion';
 var urlsToCache = [
   '/',
-  '/estilos.css',
+  '/css/estilos.css',
   '/script.js',
-  '/fontello.css',
-  '/tesoem.css',
+  '/font/fontello.css',
+  '/css/tesoem.css',
   '/manifest.json'
   
   
@@ -35,54 +35,21 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
-//cuando el navegador recupera una url
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-          
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
 
 
-self.addEventListener('activate', function(event) {
+//self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+ // var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
 
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+ // event.waitUntil(
+  //  caches.keys().then(function(cacheNames) {
+  //    return Promise.all(
+   //     cacheNames.map(function(cacheName) {
+   //       if (cacheWhitelist.indexOf(cacheName) === -1) {
+     //       return caches.delete(cacheName);
+     //     }
+    //    })
+    //  );
+   // })
+ // );
+//});
